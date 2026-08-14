7b1e8141f0767806023bf5c242794917c8b0e062
sadat006363 <sjmousavi0063@gmail.com>
2026-08-15 01:47:12 +0330

    Fix: replace all 'radix-ui' imports with correct @radix-ui packages

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,11 +1,10 @@
 "use client"
 
 import * as React from "react"
-import { Dialog as DialogPrimitive } from "radix-ui"
-
+import * as DialogPrimitive from "@radix-ui/react-dialog"  // ✅ صحیح
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
-import { XIcon } from "lucide-react"
+import { X } from "lucide-react"
 
 function Dialog({
   ...props
@@ -74,8 +73,7 @@ function DialogContent({
               className="absolute top-2 right-2"
               size="icon-sm"
             >
-              <XIcon
-              />
+              <X />
               <span className="sr-only">Close</span>
             </Button>
           </DialogPrimitive.Close>
